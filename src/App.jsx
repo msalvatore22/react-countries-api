9d50ebb1ef38f92ff882d5ddbecb482587fe078f
Michael Salvatore <msalvatore22@gmail.com>
2023-06-05 10:57:11 -0400

    Added option to reset filter to none

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import './App.css'
 import Countries from './features/countries/Countries'
 import Search from './features/search/Search'
 import Filter from './features/filter/Filter'
-import { useSelector } from 'react-redux'
 
 function App() {
-  const filter = useSelector(state => state.filter)
-
   return (
     <>
       <NavBar />
@@ -19,7 +16,6 @@ function App() {
           </Col>
           <Col>
             <Filter />
-            <p>current filter is {filter.filter}</p>
           </Col>
         </Row>
         <Row>
--- src/features/filter/Filter.jsx
+++ src/features/filter/Filter.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { setFilter } from './filterSlice'
-const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"]
-
-
+const regions = ["None", "Africa", "Americas", "Asia", "Europe", "Oceania"]
 
 const Filter = () => {
   const dispatch = useDispatch()
+
+  const handleDropDownClick = (region) => {
+
+    if(region === "None"){
+      dispatch(setFilter(''))
+    } else {
+      dispatch(setFilter(region))
+    }
+  }
 
   return (
     <DropdownButton id="dropdown-basic-button" title="Filter by Region">
       {
         regions.map(region => {
-          return <Dropdown.Item key={region} onClick={() => dispatch(setFilter(region))}>{region}</Dropdown.Item>
+          return <Dropdown.Item key={region} onClick={() => handleDropDownClick(region)}>{region}</Dropdown.Item>
         })
       }
     </DropdownButton>
